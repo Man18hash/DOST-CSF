5c70f9ffc7586a6135b963880c6b0019d6ede155
olivemagmoyao <omagmoyao@gmail.com>
2025-02-14 16:23:04 +0800

    vday2

--- resources/views/feedback_form.blade.php
+++ resources/views/feedback_form.blade.php
@@ -6,7 +6,11 @@
     <div class="content-wrapper">
         <!-- Title Section -->
         <div class="form-container1">
-            <h1 class="title-text">CUSTOMER SATISFACTION FEEDBACK</h1>
+            <h1 style="font-size: 50px" class="title-text">CUSTOMER SATISFACTION FEEDBACK</h1>
+            <h1 style="text-transform: capitalize; letter-spacing: 1px; font-weight:normal" class="title-text">Pagkuntento ng Pagsilbihan</h1>
+        </br>
+            <h3 style="letter-spacing: 1px" class="title-text"><i>HELP US SERVE YOU BETTER!</i></h3>
+            <h3 style="text-transform: capitalize; letter-spacing: 1px; font-weight:normal" class="title-text"><i>Tulungan mo kaming mas mapabuti and aming mga proseso at serbisyo!</i></h3>
         </div>
 
         <!-- Form Section -->
@@ -14,6 +18,26 @@
             <form method="POST" action="{{ route('feedback.store') }}" class="feedback-form">
                 @csrf
                 <table class="feedback-table">
+                    <tr>
+                        <td colspan="2" style="text-align: center; font-weight: bold; padding: 15px; background: rgba(38, 58, 156, 0.1);">
+                            The Client Satisfaction Measurement (CSM) tracks the customer experience of government offices.
+                            Your feedback on your recently concluded transaction will help this office provide a better service.
+                            Personal information shared will be kept strictly confidential.
+                            <p style="font-weight:normal">[Ang Client Satisfaction Measurement (CSM) ay naglalayong masubaybayan ang
+                                karanasan ng taumbayan hinggil sa kanilang pakikitransaksyon sa mga tanggapan ng gobyerno. Makakatulong
+                                ang inyong kasagutan ukol sa inyong naging karanasan sa katatapos lamang na transaksyon, upang mas mapabuti
+                                at lalong mapahusay ang aming serbisyo publiko. Ang personal na impormasyson na iyong ibabahagi ay mananatiling
+                                kumpidensyal.]</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="text-align: left; font-weight: bold; padding: 15px; background: rgba(38, 58, 156, 0.1);">
+                            INSTRUCTIONS: Write legibly or check mark (✔) on the following basic information. <p style="font-weight:normal">[Sumulat ng malinaw o lagyan ng tsek (✔) sa sumusunod na pangunahing impormasyon.]</p>
+                        </td>
+                    </tr>
                     <tr>
                         <td><strong>Name:</strong></td>
                         <td><input type="text" name="name" placeholder="Optional"></td>
