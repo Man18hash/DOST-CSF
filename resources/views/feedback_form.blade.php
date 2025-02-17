d2e0750fb9413eb7a9d66c50c123002c4a85818d
olivemagmoyao <omagmoyao@gmail.com>
2025-02-17 13:49:03 +0800

    Feb 17

--- resources/views/feedback_form.blade.php
+++ resources/views/feedback_form.blade.php
@@ -35,7 +35,8 @@
                     </tr>
                     <tr>
                         <td colspan="2" style="text-align: left; font-weight: bold; padding: 15px; background: rgba(38, 58, 156, 0.1);">
-                            INSTRUCTIONS: Write legibly or check mark (✔) on the following basic information. <p style="font-weight:normal">[Sumulat ng malinaw o lagyan ng tsek (✔) sa sumusunod na pangunahing impormasyon.]</p>
+                            INSTRUCTIONS: Write legibly or check mark (✔) on the following basic information.
+                            <span style="font-weight:normal">[Sumulat ng malinaw o lagyan ng tsek (✔) sa sumusunod na pangunahing impormasyon.]</span>
                         </td>
                     </tr>
                     <tr>
@@ -81,6 +82,239 @@
                         <td><strong>Date:</strong></td>
                         <td><input type="date" name="date" required></td>
                     </tr>
+                    <tr>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="text-align: left; font-weight: bold; padding: 15px; background: rgba(38, 58, 156, 0.1);">
+                            INSTRUCTIONS: Check mark (✔) <span style="font-weight:normal">on your answers to the question on the Citizen's Charter (CC). It is a document
+                                detailing a government agency's services, requirements, fees, and processing times.</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>CC1: Which of the following best describes your awareness of a CC?</strong></td>
+                        <td class="checkbox-group">
+                            <label><input type="checkbox" name="CC1" value="I know what a CC is and I saw this office's CC."> I know what a CC is and I saw this office's CC.</label>
+                            <label><input type="checkbox" name="CC1" value="I know what a CC is but I did not see the office's CC."> I know what a CC is but I did not see the office's CC.</label>
+                            <label><input type="checkbox" name="CC1" value="I learned of the CC only when I saw this office's CC."> I learned of the CC only when I saw this office's CC.</label>
+                            <label><input type="checkbox" name="CC1" value="I do not know what a CC is and I did not see one in this office."> I do not know what a CC is and I did not see one in this office.</label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>CC2: If aware of CC (answered 1-3 in CC1), would you say that the CC of this office was ...?</strong></td>
+                        <td class="checkbox-group">
+                            <label><input type="checkbox" name="CC2" value="Easy to see"> Easy to see</label>
+                            <label><input type="checkbox" name="CC2" value="Somewhat easy to see"> Somewhat easy to see</label>
+                            <label><input type="checkbox" name="CC2" value="Difficult to see"> Difficult to see</label>
+                            <label><input type="checkbox" name="CC2" value="Not visible at all"> Not visible at all</label>
+                            <label><input type="checkbox" name="CC2" value="Not applicable"> Not applicable</label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>CC3: If aware of CC (answered 1-3 in CC1), how much did the CC help you in your transaction?</strong></td>
+                        <td class="checkbox-group">
+                            <label><input type="checkbox" name="CC3" value="Helped very much"> Helped very much</label>
+                            <label><input type="checkbox" name="CC3" value="Somewhat helped"> Somewhat helped</label>
+                            <label><input type="checkbox" name="CC3" value="Did not helped"> Did not helped</label>
+                            <label><input type="checkbox" name="CC3" value="Not applicable"> Not applicable</label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="text-align: left; font-weight: bold; padding: 15px; background: rgba(38, 58, 156, 0.1);">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>DOST 02 Office/Unit Provider:</strong></td>
+                        <td><input type="text" name="unit provider" required></td>
+                    </tr>
+                    <tr>
+                        <td><strong>Service/Transaction/Assistance Availed:</strong></td>
+                        <td><input type="text" name="assistance availed" required></td>
+                    </tr>
+                    <tr>
+                        <td><strong>Name of DOST 02 Employee:</strong></td>
+                        <td><input type="text" name="DOST Employee" required></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="text-align: left; font-weight: bold; padding: 15px; background: rgba(38, 58, 156, 0.1);">
+                            INSTRUCTIONS:<span style="font-weight:normal"> For SQD 0-8, please put a</span>
+                            <span style="font-weight: bold"> check mark (✔)</span>
+                            <span style="font-weight:normal"> on the column that best corresponds to your answer.</span>
+                            <div style="margin-top: 5px;">
+                                <p style="font-weight: bold">❌ - STRONGLY DISAGREE (SD)</p>
+                                <p style="font-weight: bold">😕 - DISAGREE (D)</p>
+                                <p style="font-weight: bold">😐 - NEITHER AGREE NOR DISAGREE (NAD)</p>
+                                <p style="font-weight: bold">🙂 - AGREE (A)</p>
+                                <p style="font-weight: bold">😍 - STRONGLY AGREE (SA)</p>
+                            </div>
+                        </td>
+                    </tr>
+                    <table class="criteria-table">
+                        <tr>
+                            <th colspan="7" class="table-header">CRITERIA</th>
+                        </tr>
+                        <tr>
+                            <th class="question-header">Questions</th>
+                            <th class="rating-header">❌<br><span>1</span></th>
+                            <th class="rating-header">😕<br><span>2</span></th>
+                            <th class="rating-header">😐<br><span>3</span></th>
+                            <th class="rating-header">🙂<br><span>4</span></th>
+                            <th class="rating-header">😍<br><span>5</span></th>
+                            <th class="rating-header">❓<br><span>N/A</span></th>
+                        </tr>
+
+                        <tr>
+                            <td><strong>SQD0.</strong> <span style="font-weight: normal">I am satisfied with the service that I availed.</span></td>
+                            <td><input type="radio" name="SQD0" value="1"></td>
+                            <td><input type="radio" name="SQD0" value="2"></td>
+                            <td><input type="radio" name="SQD0" value="3"></td>
+                            <td><input type="radio" name="SQD0" value="4"></td>
+                            <td><input type="radio" name="SQD0" value="5"></td>
+                            <td><input type="radio" name="SQD0" value="N/A"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>SQD1.</strong> <span style="font-weight: normal">I spent a reasonable amount of time for my transaction.</span></td>
+                            <td><input type="radio" name="SQD1" value="1"></td>
+                            <td><input type="radio" name="SQD1" value="2"></td>
+                            <td><input type="radio" name="SQD1" value="3"></td>
+                            <td><input type="radio" name="SQD1" value="4"></td>
+                            <td><input type="radio" name="SQD1" value="5"></td>
+                            <td><input type="radio" name="SQD1" value="N/A"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>SQD2.</strong> <span style="font-weight: normal">The office followed the transaction's requirements and steps based on the information provided.</span></td>
+                            <td><input type="radio" name="SQD2" value="1"></td>
+                            <td><input type="radio" name="SQD2" value="2"></td>
+                            <td><input type="radio" name="SQD2" value="3"></td>
+                            <td><input type="radio" name="SQD2" value="4"></td>
+                            <td><input type="radio" name="SQD2" value="5"></td>
+                            <td><input type="radio" name="SQD2" value="N/A"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>SQD3.</strong> <span style="font-weight: normal">The steps (including payment) I needed to do for my transaction were easy and simple.</span></td>
+                            <td><input type="radio" name="SQD3" value="1"></td>
+                            <td><input type="radio" name="SQD3" value="2"></td>
+                            <td><input type="radio" name="SQD3" value="3"></td>
+                            <td><input type="radio" name="SQD3" value="4"></td>
+                            <td><input type="radio" name="SQD3" value="5"></td>
+                            <td><input type="radio" name="SQD3" value="N/A"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>SQD4.</strong> <span style="font-weight: normal">I easily found information about my transaction from the office or its website.</span></td>
+                            <td><input type="radio" name="SQD4" value="1"></td>
+                            <td><input type="radio" name="SQD4" value="2"></td>
+                            <td><input type="radio" name="SQD4" value="3"></td>
+                            <td><input type="radio" name="SQD4" value="4"></td>
+                            <td><input type="radio" name="SQD4" value="5"></td>
+                            <td><input type="radio" name="SQD4" value="N/A"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>SQD5.</strong> <span style="font-weight: normal">I paid a reasonable amount of fees for my transaction. (Cost)</span></td>
+                            <td><input type="radio" name="SQD5" value="1"></td>
+                            <td><input type="radio" name="SQD5" value="2"></td>
+                            <td><input type="radio" name="SQD5" value="3"></td>
+                            <td><input type="radio" name="SQD5" value="4"></td>
+                            <td><input type="radio" name="SQD5" value="5"></td>
+                            <td><input type="radio" name="SQD5" value="N/A"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>SQD6.</strong> <span style="font-weight: normal">I feel the office was fair to everyone, or "walang palakasan", during my transaction.</span></td>
+                            <td><input type="radio" name="SQD6" value="1"></td>
+                            <td><input type="radio" name="SQD6" value="2"></td>
+                            <td><input type="radio" name="SQD6" value="3"></td>
+                            <td><input type="radio" name="SQD6" value="4"></td>
+                            <td><input type="radio" name="SQD6" value="5"></td>
+                            <td><input type="radio" name="SQD6" value="N/A"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>SQD7.</strong> <span style="font-weight: normal">I was treated courteously by the staff, and (if asked for help) the staff was helpful.</span></td>
+                            <td><input type="radio" name="SQD7" value="1"></td>
+                            <td><input type="radio" name="SQD7" value="2"></td>
+                            <td><input type="radio" name="SQD7" value="3"></td>
+                            <td><input type="radio" name="SQD7" value="4"></td>
+                            <td><input type="radio" name="SQD7" value="5"></td>
+                            <td><input type="radio" name="SQD7" value="N/A"></td>
+                        </tr>
+                        <tr>
+                            <td><strong>SQD8.</strong> <span style="font-weight: normal">I got what i needed from the government office, or (if denied) denial of request was sufficiently explained to me.</span></td>
+                            <td><input type="radio" name="SQD8" value="1"></td>
+                            <td><input type="radio" name="SQD8" value="2"></td>
+                            <td><input type="radio" name="SQD8" value="3"></td>
+                            <td><input type="radio" name="SQD8" value="4"></td>
+                            <td><input type="radio" name="SQD8" value="5"></td>
+                            <td><input type="radio" name="SQD8" value="N/A"></td>
+                        </tr>
+                    </table>
+                </br>
+                    <!-- Suggestions/Comments Section -->
+                    <tr>
+                        <td colspan="7" style="text-align: left; padding: 15px;"><strong>
+                            Suggestions/Comments on how we can further improve our services (optional):</strong>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="7">
+                            <textarea name="suggestions" rows="4" style="width: 100%; padding: 10px; border: 1px solid #ccc; border-radius: 5px;"></textarea>
+                        </td>
+                    </tr>
+
+                    <!-- Likert Scale Rating -->
+                    <tr>
+                        <td colspan="7" style="text-align: center; padding: 15px;"><strong>
+                            Please put a check mark (✔), on a scale of 1 to 10,
+                            with 10 being the highest and 1 being the lowest,
+                            how likely you are to recommend our programs and services.</strong>
+                        </td>
+                    </tr>
+
+                    <!-- Rating Scale Row -->
+                    <div>
+                        <tr style="text-align: center; font-weight: bold; font-size: 18px;">
+                            <td colspan="7">
+                                <label style="color: red; padding: 10px;">
+                                    <input type="radio" name="recommendation" value="1">
+                                    <span style="border: 2px solid red; padding: 5px 10px; display: inline-block;">01</span>
+                                </label>
+                                <label style="color: red; padding: 10px;">
+                                    <input type="radio" name="recommendation" value="2">
+                                    <span style="border: 2px solid red; padding: 5px 10px; display: inline-block;">02</span>
+                                </label>
+                                <label style="color: orange; padding: 10px;">
+                                    <input type="radio" name="recommendation" value="3">
+                                    <span style="border: 2px solid orange; padding: 5px 10px; display: inline-block;">03</span>
+                                </label>
+                                <label style="color: orange; padding: 10px;">
+                                    <input type="radio" name="recommendation" value="4">
+                                    <span style="border: 2px solid orange; padding: 5px 10px; display: inline-block;">04</span>
+                                </label>
+                                <label style="color: gold; padding: 10px;">
+                                    <input type="radio" name="recommendation" value="5">
+                                    <span style="border: 2px solid gold; padding: 5px 10px; display: inline-block;">05</span>
+                                </label>
+                                <label style="color: gold; padding: 10px;">
+                                    <input type="radio" name="recommendation" value="6">
+                                    <span style="border: 2px solid gold; padding: 5px 10px; display: inline-block;">06</span>
+                                </label>
+                                <label style="color: blue; padding: 10px;">
+                                    <input type="radio" name="recommendation" value="7">
+                                    <span style="border: 2px solid blue; padding: 5px 10px; display: inline-block;">07</span>
+                                </label>
+                                <label style="color: blue; padding: 10px;">
+                                    <input type="radio" name="recommendation" value="8">
+                                    <span style="border: 2px solid blue; padding: 5px 10px; display: inline-block;">08</span>
+                                </label>
+                                <label style="color: green; padding: 10px;">
+                                    <input type="radio" name="recommendation" value="9">
+                                    <span style="border: 2px solid green; padding: 5px 10px; display: inline-block;">09</span>
+                                </label>
+                                <label style="color: green; padding: 10px;">
+                                    <input type="radio" name="recommendation" value="10">
+                                    <span style="border: 2px solid green; padding: 5px 10px; display: inline-block;">10</span>
+                                </label>
+                            </td>
+                        </tr>
+                    </div>
+
                 </table>
                 <button type="submit" class="btn-submit">Submit</button>
             </form>
